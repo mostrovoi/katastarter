3fe8c0d75dc0f701235541b4a543f93df59b597a
Oscar <oscar.perez@ext.privalia.com>
2019-10-04 14:24:03 +0200

    Change to mockito

--- src/test/kotlin/WardRobeTest.kt
+++ src/test/kotlin/WardRobeTest.kt
@@ -1,32 +1,16 @@
 import assertk.assertThat
 import assertk.assertions.isEqualTo
-import org.junit.Before
 import org.junit.Test
-import org.junit.internal.runners.model.EachTestNotifier
-import org.junit.runner.RunWith
-import org.mockito.Mock
 import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
-import org.mockito.runners.MockitoJUnitRunner
 
 class WardRobeTest {
 
-  //  @Before
-   // fun setupAccountData() {
-    //  MockitoAnnotations.initMocks(this)
-   // }
-
-    @Mock
-    lateinit var combinationMock : Combination
-
-    @Mock
-    lateinit var emailSender : EmailSender
 
     @Test
     fun should_return_all_valid_drawers() {
 
-        //val combinationMock = Mockito.mock(Combination::class.java)
-        //val emailSender = Mockito.mock(EmailSender::class.java)
+        val combinationMock = Mockito.mock(Combination::class.java)
+        val emailSender = Mockito.mock(EmailSender::class.java)
 
         val potato = PotatoBasket(total = 0)
         Mockito.`when`(combinationMock.combine()).thenReturn(3);
